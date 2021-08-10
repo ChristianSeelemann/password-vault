d87075f94b3d7fbe95ba5de86dd8bf2897dc5fc5
ChristianSeelemann <cseelemann@gmx.de>
2021-08-10 14:05:09 +0200

    🗃️ Read data from db.json

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-console.log('Hello Server!');
+import fs from 'fs/promises';
+
+async function readDB() {
+  try {
+    const db = fs.readFile('src/db.json', 'utf-8');
+    console.log(await db);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+readDB();
