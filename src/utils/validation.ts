60ebd5174a842818d52a03936b4be5543d6061e7
ChristianSeelemann <cseelemann@gmx.de>
2021-08-18 14:53:37 +0200

    💡 Delete comments

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -4,10 +4,8 @@ import CryptoJS from 'crypto-js';
 export async function validateMasterpassword(
   password: string
 ): Promise<boolean> {
-  // read hashedMasterpassword from .password
   const hashedMasterpassword = await readFile('.password', 'utf-8');
-  // hash password argument
   const hashedPassword = CryptoJS.SHA256(password).toString();
-  // compare hashedMasterpassword with password and return the outcome
+
   return hashedMasterpassword === hashedPassword;
 }
